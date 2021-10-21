6841ae1ca7616e6650fc1aa5346773590aef1fe9
dhemz21 <omapasdhemz77.com>
2021-10-21 02:07:02 -0500

    first commit

---
+++ jquery/js.js
@@ -0,0 +1,27 @@
+function myFunction(){
+    document.getElementById("demo") .innerHTML = "<strong>Dhemler A. Omapas</strong>";
+};
+function myFunction1(){
+    document.getElementById("demo1") .innerHTML = "<strong>December 1, 1998</strong>";
+};
+function myFunction2(){
+    document.getElementById("demo2") .innerHTML = "<strong>Brgy. Cabintan Ormoc City</strong>";
+};
+function myFunction3(){
+    document.getElementById("demo3") .innerHTML = "<strong>Single</strong>";
+};
+function myFunction4(){
+    document.getElementById("demo4") .innerHTML = "<strong>165cm</strong>";
+};
+function myFunction5(){
+    document.getElementById("demo5") .innerHTML = "<strong>65kg</strong>";
+};
+function myFunction6(){
+    document.getElementById("demo6") .innerHTML = "<strong>Roman Catholic</strong>";
+};
+function myFunction7(){
+    document.getElementById("demo7") .innerHTML = "<strong>Filipino</strong>";
+};
+function myFunction8(){
+    document.getElementById("demo8") .innerHTML = "<strong>omapasdhemz77@gmail.com</strong>";
+};
